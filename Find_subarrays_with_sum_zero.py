9ebef715b439ac0fd452e3069f4680d8a321b34d
Sai Sandeep Reddy <117850316+SandyK1002@users.noreply.github.com>
2024-09-15 00:57:30 +0530

    Update Find_subarrays_with_sum_zero.py

--- Find_subarrays_with_sum_zero.py
+++ Find_subarrays_with_sum_zero.py
@@ -1,19 +1,19 @@
-import numpy as np
-import math
 arr=[1, 2, -3, 3, -1, 2]
+# arr=[4, -1, -3, 1, 2, -1]
 # arr=[-3, 1, 2, -3, 4, 0]
-# arr= [4, -1, -3, 1, 2, -1]
-ans=[sum(arr[:i+1]) for i in range(len(arr))]
-sol=[]
-print(ans)
-if ans.index(0):
-    sol.append(tuple([0,ans.index(0)]))
-for i in range(len(ans)):
-    if ans.count(ans[i])==2:
-        result = np.where(np.array(ans) == ans[i])[0]
-        sol.append(tuple(result))
-    if ans.count(ans[i])>2:
-        result = np.where(np.array(ans) == ans[i])[0]
-        sol.append(tuple(result[0:1]))
-    ans[i]=math.inf
-print(sol)
+prefix_sum_map = {}
+result = []
+cumulative_sum = 0
+   
+for i in range(len(arr)):
+    cumulative_sum += arr[i]
+    if cumulative_sum == 0:
+        result.append((0, i))
+    if cumulative_sum in prefix_sum_map:
+        for start_index in prefix_sum_map[cumulative_sum]:
+            result.append((start_index + 1, i))        
+    if cumulative_sum in prefix_sum_map:
+        prefix_sum_map[cumulative_sum].append(i)
+    else:
+        prefix_sum_map[cumulative_sum] = [i]
+print(result)
